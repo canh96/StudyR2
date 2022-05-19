bf53a94a5888ca4bf5f7dbcb0f0b455619eab2be
canh96 <ehdgus662@gmail.com>
2022-05-19 17:37:44 +0900

    220519 커밋

--- ORACLO 연동.R
+++ ORACLO 연동.R
@@ -1,23 +1,26 @@
-##Oracle연결 
-#한글 해결 = DBMSencoding = 'euc-kr' not 'utf-8'
-#RJAVA, RJDBC, rOracle
-#1.RODBC패키지 설치
-install.packages('RODBC')
-                 
-#Java DB Connectity
-#2.Library 연결
+## Oracle연결 1. RODBC 패키지 설치
+## 한글해결 DBMSencoding = 'euc-kr' not 'utf-8'
+## Rjava, rjdbc, rOracle
+install.packages('RODBC') # ROacle Open DataBase Connectity 
+# Java DB Connectity
+## 2. Library 연결
 library(RODBC)
 
-#ODBC 데이터원본(64비트) 설정
-conn1 = odbcConnect('SCOTT_dsn', uid = 'scott', pwd = 'tiger',
+## 3. ODBC 데이터원본(64비트) 설정
+conn1 = odbcConnect('SCOTT_DSN', uid = 'scott', pwd = 'tiger', 
                     DBMSencoding = 'euc-kr',
                     believeNRows = F)
 
-res<- sqlQuery(conn1, 'SELECT *FROm dept')
+## 4. 연결확인
+summary(conn1)
+
+## 5. 쿼리 실행
+res <- sqlQuery(conn1, 'SELECT * FROM dept')
 str(res)
 
-sqlQuery(conn1, 'SELECT *FROm MEMBERTBL')
+sqlQuery(conn1, 'SELECT * FROM emp')
 
-sqlQuery(conn1, 'SELECT *FROm emp')
+res <- sqlQuery(conn1, 'SELECT * FROM memberTBL')
 
-queryClose()
+## 6. 접속
+odbcClose(conn1)
